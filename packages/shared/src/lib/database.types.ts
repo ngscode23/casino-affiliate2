f5b2a2bfd482f410200920c5934b9ce442254211
ngscode23 <stasvolohovish@gmail.com>
2025-11-01 23:06:41 +0100

    supabase gen types

--- packages/shared/src/lib/database.types.ts
+++ packages/shared/src/lib/database.types.ts
@@ -735,7 +735,7 @@ export type Database = {
           shipping_total: number
           status: Database["public"]["Enums"]["order_status"]
           subtotal: number
-          user_id: string | null
+          user_id: string
         }
         Insert: {
           amount_cents?: number | null
@@ -754,7 +754,7 @@ export type Database = {
           shipping_total?: number
           status?: Database["public"]["Enums"]["order_status"]
           subtotal?: number
-          user_id?: string | null
+          user_id: string
         }
         Update: {
           amount_cents?: number | null
@@ -773,7 +773,7 @@ export type Database = {
           shipping_total?: number
           status?: Database["public"]["Enums"]["order_status"]
           subtotal?: number
-          user_id?: string | null
+          user_id?: string
         }
         Relationships: []
       }
